4d198813c045f5f5f6db80c1e598dd7db1467114
Jameson <james.bradlee@telenor.no>
2023-10-31 09:31:40 +0100

    suprres unused in style parser

--- color-env/src/main/kotlin/no/telenor/kt/color/parsers/StyleParser.kt
+++ color-env/src/main/kotlin/no/telenor/kt/color/parsers/StyleParser.kt
@@ -1,5 +1,3 @@
-@file:Suppress("UNUSED_PARAMETER", "unused")
-
 package no.telenor.kt.color.parsers
 
 import no.telenor.kt.color.Styles
@@ -7,5 +5,6 @@ import no.telenor.kt.env.Parser
 import kotlin.reflect.KType
 
 class StyleParser : Parser {
+	@Suppress("UNUSED_PARAMETER", "unused")
 	fun parseStyles(type: KType, name: String, value: String): Styles = Styles.parse(value)
 }
