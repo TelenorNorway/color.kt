ce96ee058b7016e5a21fd1891d7ed2fbc6f6906e
Jameson <james.bradlee@telenor.no>
2023-11-11 17:15:34 +0100

    chore: bump env.kt to 0.6.1

--- color-env/build.gradle.kts
+++ color-env/build.gradle.kts
@@ -4,7 +4,7 @@ repositories {
 
 dependencies {
 	compileOnly(project(":color"))
-	implementation("no.telenor.kt:env:0.6.0")
+	implementation("no.telenor.kt:env:0.6.1")
 	testImplementation("no.telenor.kt:setenv:2.0.1")
 	testImplementation(project(":color"))
 }
